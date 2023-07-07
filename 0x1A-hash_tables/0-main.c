d0379917abb6ebd0d9b5d73835f78fb4fca93971
Ayoub-kf7 <ayoub98koufi@gmail.com>
2023-07-07 11:05:46 -0700

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/0-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
